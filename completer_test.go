197093f8c169680fb46ab74dfd1f2779db14be5e
Andrew Bates <abates@omeganetserv.com>
2015-02-18 21:36:50 -0500

    Fixing license typo

--- completer_test.go
+++ completer_test.go
@@ -1,7 +1,7 @@
 /**
- * = LICENSE
+ * Copyright 2015 Andrew Bates
  *
- * Copyright 2015 Andrew Bates Licensed under the Apache License, Version 2.0
+ * Licensed under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with the
  * License. You may obtain a copy of the License at
  *
