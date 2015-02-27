e7ece80032190eecdc1a0e3c9b2920d7b70e3bab
Andrew Bates <abates@omeganetserv.com>
2015-02-27 09:13:03 -0500

    Added documentation

--- lineEditor.go
+++ lineEditor.go
@@ -20,14 +20,21 @@ import (
 	"github.com/peterh/liner"
 )
 
+// LineEditor wraps the basic Prompt method
 type LineEditor interface {
 	Prompt(string) (string, error)
 }
 
+// DefaultLineEditor is a concrete implementation of LineEditor that uses
+// github.com/peterh/liner as the line editor
 type DefaultLineEditor struct {
 	liner *liner.State
 }
 
+// Prompt will prompt the user with the prompt string, collect the response and
+// return it.  If the upstream liner.Prompt function succeeds, then the
+// response is added to the history.  The collected string and any associated
+// error is returned
 func (d *DefaultLineEditor) Prompt(prompt string) (string, error) {
 	str, err := d.liner.Prompt(prompt)
 	if err == nil {
@@ -36,10 +43,14 @@ func (d *DefaultLineEditor) Prompt(prompt string) (string, error) {
 	return str, err
 }
 
+// Close returns the terminal to the original state.  This includes taking the
+// terminal out of raw mode and turning echo back on
 func (d *DefaultLineEditor) Close() error {
 	return d.liner.Close()
 }
 
+// NewDefaultLineEditor returns a fully initialized line editor that includes
+// autocompletion and history
 func NewDefaultLineEditor(commands CommandMap) *DefaultLineEditor {
 	l := liner.NewLiner()
 	l.SetTabCompletionStyle(liner.TabPrints)
