db4f274302cc3e7038237d6cf8ce2cecca8b105c
Andrew Bates <abates@omeganetserv.com>
2015-02-27 09:30:56 -0500

    Documentation

--- prompt.go
+++ prompt.go
@@ -18,21 +18,43 @@ package gosh
 
 import ()
 
+// Closeable objects implement a Close method.
+//
+// If the prompt detects that the line editor implements Closeable then it will
+// call Close upon completing the prompt cycle
 type Closeable interface {
 	Close() error
 }
 
+// Prompt receivers implement a NextResponse method
+//
+// NextResponse should return any response provided by the user as well as any
+// error encountered during the prompting
 type Prompt interface {
 	NextResponse() (string, error)
 }
 
+// Prompter returns the prompt string used in NextResponse
+//
+// Prompter is a function that is called to generate the prompt string that
+// will preced user input.  For instance, if a Prompter returns the string "> "
+// then all input lines will begin with that string and user input starts just
+// after the space following the ">"
 type Prompter func() string
 
+// DefaultPrompt is a concrete implementation of Prompt
+//
+// DefaultPrompt includes a basi "> " prompt and the DefaultLineEditor which
+// provides tab completion and command history
 type DefaultPrompt struct {
 	prompter   Prompter
 	lineEditor LineEditor
 }
 
+// NewDefaultPrompt returns a fully initialized DefaultPrompt.
+//
+// When NextResponse is called on the DefaultPrompt the prompt will be "> ".
+// The DefaultLineEditor is used so tab completions and history is available
 func NewDefaultPrompt(commands CommandMap) *DefaultPrompt {
 	p := DefaultPrompt{
 		func() string {
@@ -43,6 +65,11 @@ func NewDefaultPrompt(commands CommandMap) *DefaultPrompt {
 	return &p
 }
 
+// SetPrompter will allow overriding the default prompter.
+//
+// If the prompter argument is nil then the prompter is not overridden and the
+// ErrNilCallback is returned.  Otherwise the prompter is set to whatever
+// function is provided
 func (p *DefaultPrompt) SetPrompter(prompter func() string) error {
 	if prompter == nil {
 		return ErrNilCallback
@@ -51,6 +78,12 @@ func (p *DefaultPrompt) SetPrompter(prompter func() string) error {
 	return nil
 }
 
+// SetLineEditor allows overriding the default line editor
+//
+// The DefaultPrompt uses github.com/peterh/liner as the default line editor.
+// This can be overridden with a different LineEditor implementation.  If
+// SetLineEditor is called with a nil LineEditor then the ErrNilLineEditor
+// error is returned
 func (p *DefaultPrompt) SetLineEditor(lineEditor LineEditor) error {
 	if lineEditor == nil {
 		return ErrNilLineEditor
@@ -59,10 +92,12 @@ func (p *DefaultPrompt) SetLineEditor(lineEditor LineEditor) error {
 	return nil
 }
 
+// NextResponse prompts the user and returns the uer's input
 func (p *DefaultPrompt) NextResponse() (string, error) {
 	return p.lineEditor.Prompt(p.prompter())
 }
 
+// Close closes the line editor (if it is closeable)
 func (p *DefaultPrompt) Close() error {
 	if lineEditor, ok := p.lineEditor.(Closeable); ok {
 		return lineEditor.Close()
